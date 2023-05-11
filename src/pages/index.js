089deee9be278e0a73416e19e86f682a77d119a4
andersoncsousa <caique_sousa@outlook.com.br>
2023-05-11 05:16:54 -0300

    add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <Head>
         <title>CS Tech</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel="icon" type="image/png" sizes="16x16"  href="cs-favicon.ico" />
       </Head>
       <Navbar />
       <BtnBack />
